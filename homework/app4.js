118cf89b1cd5139a1a1853032aca8dc73ec29f42
Hoang Viet Anh <110024404+HoangVietAnh198@users.noreply.github.com>
2022-07-28 22:17:06 +0700

    Add files via upload

---
+++ homework/app4.js
@@ -0,0 +1,20 @@
+// bài 1:
+// let a = prompt("Nhập điểm môn Vật Lý");
+// let b = prompt("Nhập điểm môn Hóa Học");
+// let c = prompt("Nhập điểm môn Sinh Học");
+
+// let e = Number(a)+Number(b)+Number(c);
+// let d = e/3;
+// alert(`Điểm trung bình là ${d}
+// Điểm tổng là ${e}`);
+// bài 2:
+// let g = prompt("nhập độ C");
+// alert(`độ F sẽ là ${g*1.8+32}`);
+// bài 3:
+// let pi = Math.PI
+// let f = prompt("nhập độ dài bán kính hình tròn");
+// alert(`Diện tích hình tròn là : ${f*f*pi}`)
+// bài 4:
+// let pi = Math.PI
+// let g = prompt("nhập độ dài bán kính hình tròn");
+// alert(`Chu vi hình tròn là : ${2*g*pi}`)
